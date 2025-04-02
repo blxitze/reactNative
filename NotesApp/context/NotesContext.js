915d959d30bb0d354c9f71f9a243e6105cdb8e2e
ayo <150446051+ayan1232008@users.noreply.github.com>
2025-04-02 22:35:38 +0500

    Update NotesContext.js

--- NotesApp/context/NotesContext.js
+++ NotesApp/context/NotesContext.js
@@ -1,6 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
 export const NotesContext = createContext();
 
 export const NotesProvider = ({ children }) => {
